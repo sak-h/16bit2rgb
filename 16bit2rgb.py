adbce4f83fcdf8c2c8276cb1cff308afbb4356a0
Sak.H <a@a.com>
2021-06-20 12:53:30 +0100

    add the script file

---
+++ 16bit2rgb.py
@@ -0,0 +1,50 @@
+import sys,os
+import argparse
+import glob 
+import numpy as np
+import cv2
+from matplotlib import cm
+
+
+parser = argparse.ArgumentParser(description='convert_images.py')
+parser.add_argument('input_files', help='Target Image Files')
+parser.add_argument('--out', '-o', default='', help='Output Dir')
+parser.add_argument('--min_val', type=int, default=-1, help='Min value')
+parser.add_argument('--max_val', type=int, default=-1, help='Max value')
+parser.add_argument('--map', '-m', default='CMRmap', help='Colour Map')
+
+args = parser.parse_args()
+
+if hasattr(cm, args.map):
+    cmap = getattr(cm, args.map)
+else:
+    raise NotImplementedError(args.map)
+
+max_val = 0 if args.max_val < 0 else args.max_val
+min_val = 255**2 if args.min_val < 0 else args.min_val
+
+if args.max_val < 0 and args.min_val < 0:
+    print('finding minmax values')
+    for filepath in glob.glob(args.input_files):
+        print(' target file: ' + filepath)
+        img = cv2.imread(filepath, cv2.IMREAD_ANYDEPTH)
+        ma = img.max()
+        mi = img.min()
+        if ma>max_val:
+            max_val = ma
+        if mi<min_val:
+            min_val = mi
+
+print('converting files')
+for filepath in glob.glob(args.input_files):
+    print(' target file: ' + filepath)
+    img = cv2.imread(filepath, cv2.IMREAD_ANYDEPTH)
+    img = (img-min_val).astype('float64') / (max_val-min_val)
+    img2 = (cmap(img) * 255).astype('uint8')
+    img2 = cv2.cvtColor(img2, cv2.COLOR_RGB2BGR)
+    filename = os.path.basename(filepath).replace('.tiff','.png')
+    output_filename = os.path.join(args.out, filename)
+    print('  saving: ' + output_filename)
+    cv2.imwrite(output_filename, img2)
+
+print('done (max: %d, min: %d)' % (max_val, min_val))
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # 16bit2rgb
-Script for 16bit TIFF to 24bit RGB PNG
+A python script for 16 bit TIFF to 24bit RGB PNG conversion
+
+Accepting 16 bit .tiff files (ex. [the FLIR thermal dataset](https://www.flir.com/oem/adas/adas-dataset-form/)), the script outputs 24 bit RGB .png files.
+
+## Usage
+```
+python 16bit2rgb.py INPUT_FILE_PATH --out OUTPUT_DIR
+```
+You can use the following options:
+- --out specify the output location (default: current directory)
+- --map specify the colour map named by matplotlib (default: CMRmap)
+- --min_val specify the minimum pixel value
+- --max_val specify the maximum pixel value
+
+If you specify both --min_val and --max_val, it skip to find those values. Otherwise, it tries to find those values in the specified files.
